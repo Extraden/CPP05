0af231eafac83588b892ae966de46bcdce4fcc71
dsemenov <dsemenov@student.42lyon.fr>
2026-03-03 20:11:54 +0100

    feat: add output message for Bureaucrat assignment operator

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -19,6 +19,7 @@ Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name),
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
 {
+  std::cout << "Assignment operator of Bureaucrat is called!\n";
   if (this != &other)
     this->_grade = other._grade;
   return (*this);
