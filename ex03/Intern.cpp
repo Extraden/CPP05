d0ea3f518a2abb299c36fa7a479d8c0355ba5fe3
dsemenov <dsemenov@student.42lyon.fr>
2026-03-11 22:41:06 +0100

    init ex03

---
+++ ex03/Intern.cpp
@@ -0,0 +1,23 @@
+#include "Intern.hpp"
+
+
+Intern::Intern()
+{
+}
+
+Intern::Intern(const Intern& other)
+{
+	(void) other;
+}
+
+Intern::~Intern()
+{
+}
+
+Intern& Intern::operator=(const Intern& other)
+{
+	if (this != &other)
+	{
+	}
+	return (*this);
+}
---
+++ ex03/Intern.hpp
@@ -0,0 +1,12 @@
+#ifndef INTERN_HPP
+# define INTERN_HPP
+
+class Intern
+{
+	Intern();
+	Intern(const Intern& other);
+	~Intern();
+	Intern& operator=(const Intern& other);
+};
+
+#endif
